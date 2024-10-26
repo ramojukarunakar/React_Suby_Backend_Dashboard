f0ccf019ab6637ab80fc7f02ebafdbae3628263e
karunakar <karunakarramoju143@gmail.com>
2024-10-26 12:21:55 +0530

    changed path to live in api_url

--- dashboard/src/vendorDashboard/data/apiPath.js
+++ dashboard/src/vendorDashboard/data/apiPath.js
@@ -1,4 +1,4 @@
 
-export const API_URL = "http://localhost:4000"
+//export const API_URL = "http://localhost:4000"
 
- //const API_URL = "https://backend-nodejs-suby-r9vw.onrender.com"
+ const API_URL = "https://backend-nodejs-suby-r9vw.onrender.com"
